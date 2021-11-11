2b4892cd17ee2d5785db26b22edcc0565f483e65
AndreGartner <andre.gartner@premiersoft.net>
2021-11-11 17:46:02 -0300

    Finalized layout and started countdown/challenges bussiness logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp;
